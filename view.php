aaf9a856bf284a01937bde8c20d84463d9e4914f
ranggaggngntt <ranggaakb48@yahoo.com>
2024-06-27 14:25:25 -0700

    Tambah SVG file

--- view.php
+++ view.php
@@ -1,14 +1,14 @@
 <?php 
 include './template/header.php';
 
-if (isset($_GET['id_pesanan'])) {
-    $id_pesanan = $_GET['id_pesanan'];
+if (isset($_GET['idp'])) {
+    $idp = $_GET['idp'];
 
-    $query_pelanggan = mysqli_query($connection, 
-        "SELECT * FROM pesanan p, pelanggan pl WHERE p.id_pelanggan=pl.id_pelanggan AND p.id_pesanan='$id_pesanan'");
-    $data_pelanggan = mysqli_fetch_array($query_pelanggan);
-    $nama_pelanggan = $data_pelanggan['nama_pelanggan'];
-    $id_pelanggan = $data_pelanggan['id_pelanggan'];
+    $ambilnamapelanggan = mysqli_query($connection, 
+        "SELECT * FROM pesanan p, pelanggan pl WHERE p.id_pelanggan=pl.id_pelanggan AND p.id_pesanan='$idp'");
+    $np = mysqli_fetch_array($ambilnamapelanggan);
+    $namapel = $np['nama_pelanggan'];
+    $idpel = $np['id_pelanggan'];
 } else {
     header('location:index.php');
 }
@@ -24,7 +24,7 @@ if (isset($_GET['id_pesanan'])) {
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Data Pesanan</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $id_pesanan; ?></div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $idp; ?></div>
                                 </div>
                             </div>
                         </div>
@@ -38,7 +38,7 @@ if (isset($_GET['id_pesanan'])) {
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Nama Pelanggan</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $nama_pelanggan; ?></div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $namapel; ?></div>
                                 </div>
                             </div>
                         </div>
@@ -49,7 +49,7 @@ if (isset($_GET['id_pesanan'])) {
             <div class="row">
                 <div class="col-xl-3 col-md-6">
                     <div>
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalTambahPesanan">
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
                             Tambah Pesanan
                         </button>
                     </div>
@@ -72,41 +72,32 @@ if (isset($_GET['id_pesanan'])) {
                                 <th style="width: 171px;">Aksi</th>
                             </tr>
                         </thead>
-                        <tfoot>
-                            <tr>
-                                <th>No</th>
-                                <th>Nama Produk</th>
-                                <th>Harga Satuan</th>
-                                <th>Jumlah</th>
-                                <th>Subtotal</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </tfoot>
                         <tbody>
                             <?php
-                            $query_detail_pesanan = mysqli_query(
+                            $getview = mysqli_query(
                                 $connection,
-                                "SELECT * FROM detail_pesanan dp, produk pr, pelanggan pl WHERE dp.id_produk=pr.id_produk AND id_pesanan='$id_pesanan' AND id_pelanggan='$id_pelanggan'"
+                                "SELECT * FROM detail_pesanan p, produk pr, pelanggan pl WHERE p.id_produk=pr.id_produk AND id_pesanan='$idp' AND id_pelanggan='$idpel'"
                             );
-                            $no = 1;
-                            while ($data_pesanan = mysqli_fetch_array($query_detail_pesanan)) {
-                                $id_produk = $data_pesanan['id_produk'];
-                                $id_detail_pesanan = $data_pesanan['id_detailpesanan'];
-                                $jumlah = $data_pesanan['qty'];
-                                $harga_satuan = $data_pesanan['harga'];
-                                $nama_produk = $data_pesanan['nama_produk'];
-                                $deskripsi_produk = $data_pesanan['deskripsi'];
-                                $subtotal = $jumlah * $harga_satuan;
+                            $i = 1;
+                            while ($ap = mysqli_fetch_array($getview)) {
+                                $idpr = $ap['id_produk'];
+                                $iddetail = $ap['id_detailpesanan'];
+                                $idp = $ap['id_pesanan'];
+                                $qty = $ap['qty'];
+                                $harga = $ap['harga'];
+                                $nama_produk = $ap['nama_produk'];
+                                $deskripsi = $ap['deskripsi'];
+                                $subtotal = $qty * $harga;
                             ?>
                             <tr>
-                                <td class="sorting_1"><?= $no++; ?></td>
-                                <td><?= $nama_produk; ?> (<?= $deskripsi_produk; ?>)</td>
-                                <td>Rp.<?= number_format($harga_satuan); ?></td>
-                                <td><?= number_format($jumlah); ?></td>
+                                <td class="sorting_1"><?= $i++; ?></td>
+                                <td><?= $nama_produk; ?> (<?= $deskripsi; ?>)</td>
+                                <td>Rp.<?= number_format($harga); ?></td>
+                                <td><?= number_format($qty); ?></td>
                                 <td>Rp.<?= number_format($subtotal); ?></td>
                                 <td>
-                                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modalEdit<?= $id_detail_pesanan; ?>">Edit</button> | 
-                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalHapus<?= $id_produk; ?>">Delete</button>
+                                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#edit<?= $iddetail; ?>">Edit</button> | 
+                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?= $idpr; ?>">Delete</button>
                                 </td>
                             </tr>
                             <?php 
@@ -119,8 +110,8 @@ if (isset($_GET['id_pesanan'])) {
         </div>
     </main>
     
-    <!-- Modal Tambah Pesanan -->
-    <div class="modal fade" id="modalTambahPesanan">
+    <!-- The Modal -->
+    <div class="modal fade" id="myModal">
         <div class="modal-dialog">
             <div class="modal-content">
                 <!-- Modal Header -->
@@ -134,25 +125,25 @@ if (isset($_GET['id_pesanan'])) {
                         Pilih Barang
                         <select name="id_produk" class="form-control">
                             <?php
-                            $query_produk = mysqli_query($connection, "SELECT * FROM produk");
-                            while ($data_produk = mysqli_fetch_array($query_produk)) {
-                                $id_produk = $data_produk['id_produk'];
-                                $nama_produk = $data_produk['nama_produk'];
-                                $deskripsi_produk = $data_produk['deskripsi'];
-                                $stok_produk = $data_produk['stock'];
+                            $getproduk = mysqli_query($connection, "SELECT * FROM produk");
+                            while ($pl = mysqli_fetch_array($getproduk)) {
+                                $idproduk = $pl['id_produk'];
+                                $namaproduk = $pl['nama_produk'];
+                                $deskripsi = $pl['deskripsi'];
+                                $stock = $pl['stock'];
                             ?>
-                            <option value="<?= $id_produk; ?>"><?= $nama_produk; ?> - <?= $deskripsi_produk; ?> (Stock: <?= $stok_produk; ?>)</option>
+                            <option value="<?= $idproduk; ?>"><?= $namaproduk; ?> - <?= $deskripsi; ?> (Stock: <?= $stock; ?>)</option>
                             <?php
                             }
                             ?>
                         </select>
-                        <input type="number" name="jumlah" class="form-control mt-2" placeholder="Jumlah">
-                        <input type="hidden" name="id_pesanan" value="<?= $id_pesanan; ?>">
-                        <input type="hidden" name="id_pelanggan" value="<?= $id_pelanggan; ?>">
+                        <input type="number" name="qty" class="form-control mt-2" placeholder="Jumlah">
+                        <input type="hidden" name="idp" value="<?= $idp; ?>">
+                        <input type="hidden" name="idpel" value="<?= $idpel; ?>">
                     </div>
                     <!-- Modal footer -->
                     <div class="modal-footer">
-                        <button type="submit" class="btn btn-success" name="tambah_produk">Submit</button>
+                        <button type="submit" class="btn btn-success" name="addproduk">Submit</button>
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                     </div>
                 </form>
@@ -162,17 +153,18 @@ if (isset($_GET['id_pesanan'])) {
 
     <?php
     // Loop again to generate modals
-    $query_detail_pesanan = mysqli_query(
+    $getview = mysqli_query(
         $connection,
-        "SELECT * FROM detail_pesanan dp, produk pr, pelanggan pl WHERE dp.id_produk=pr.id_produk AND id_pesanan='$id_pesanan' AND id_pelanggan='$id_pelanggan'"
+        "SELECT * FROM detail_pesanan p, produk pr, pelanggan pl WHERE p.id_produk=pr.id_produk AND id_pesanan='$idp' AND id_pelanggan='$idpel'"
     );
-    while ($data_pesanan = mysqli_fetch_array($query_detail_pesanan)) {
-        $id_produk = $data_pesanan['id_produk'];
-        $id_detail_pesanan = $data_pesanan['id_detailpesanan'];
-        $jumlah = $data_pesanan['qty'];
+    while ($ap = mysqli_fetch_array($getview)) {
+        $idpr = $ap['id_produk'];
+        $iddetail = $ap['id_detailpesanan'];
+        $idp = $ap['id_pesanan'];
+        $qty = $ap['qty'];
     ?>
-    <!-- Modal Hapus -->
-    <div class="modal fade" id="modalHapus<?= $id_produk; ?>">
+    <!-- Modal Delete -->
+    <div class="modal fade" id="delete<?= $idpr; ?>">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -182,12 +174,12 @@ if (isset($_GET['id_pesanan'])) {
                 <form method="POST">
                     <div class="modal-body">
                         Apakah Anda yakin menghapus pesanan ini?
-                        <input type="hidden" name="id_pesanan" value="<?= $id_pesanan; ?>">
-                        <input type="hidden" name="id_produk" value="<?= $id_produk; ?>">
-                        <input type="hidden" name="id_detail_pesanan" value="<?= $id_detail_pesanan; ?>">
+                        <input type="hidden" name="idp" value="<?= $idp; ?>">
+                        <input type="hidden" name="idpr" value="<?= $idpr; ?>">
+                        <input type="hidden" name="iddetail" value="<?= $iddetail; ?>">
                     </div>
                     <div class="modal-footer">
-                        <button type="submit" class="btn btn-success" name="hapus_produk_pesanan">Hapus</button>
+                        <button type="submit" class="btn btn-success" name="hapusprodukpesanan">Hapus</button>
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                     </div>
                 </form>
@@ -195,7 +187,7 @@ if (isset($_GET['id_pesanan'])) {
         </div>
     </div>
     <!-- Modal Edit -->
-    <div class="modal fade" id="modalEdit<?= $id_detail_pesanan; ?>">
+    <div class="modal fade" id="edit<?= $iddetail; ?>">
         <div class="modal-dialog">
             <div class="modal-content">
                 <!-- Modal Header -->
@@ -206,14 +198,14 @@ if (isset($_GET['id_pesanan'])) {
                 <form method="POST">
                     <!-- Modal body -->
                     <div class="modal-body">
-                        <input type="hidden" name="id_pesanan" value="<?= $id_pesanan; ?>">
-                        <input type="hidden" name="id_produk" value="<?= $id_produk; ?>">
-                        <input type="hidden" name="id_detail_pesanan" value="<?= $id_detail_pesanan; ?>">
-                        <input type="number" name="jumlah" class="form-control mt-3" placeholder="quantity" min="1" required value="<?= $jumlah; ?>">
+                        <input type="hidden" name="idp" value="<?= $idp; ?>">
+                        <input type="hidden" name="idpr" value="<?= $idpr; ?>">
+                        <input type="hidden" name="iddetail" value="<?= $iddetail; ?>">
+                        <input type="number" name="qty" class="form-control mt-3" placeholder="quantity" min="1" required value="<?= $qty; ?>">
                     </div>
                     <!-- Modal footer -->
                     <div class="modal-footer">
-                        <button type="submit" class="btn btn-success" name="edit_detail_pesanan">Edit</button>
+                        <button type="submit" class="btn btn-success" name="editdetailpesanan">Edit</button>
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button>
                     </div>
                 </form>
@@ -226,3 +218,5 @@ if (isset($_GET['id_pesanan'])) {
 
     <?php include './template/footer.php'; ?>
 </div>
+
+
